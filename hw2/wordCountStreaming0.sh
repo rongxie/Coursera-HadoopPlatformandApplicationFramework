e9869a4ece808ce9c02f4d7ad8401b129d9f83d5
Yugang Hu <yuganghu@intellidiscovery.com>
2015-11-14 02:13:37 -0700

    Add python scripts

---
+++ hw2/wordCountStreaming0.sh
@@ -0,0 +1,1 @@
+hadoop jar /opt/cloudera/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.6.0-cdh5.4.4.jar -input input -output output_new_0 -mapper wordcount_mapper.py -reducer wordcount_reducer.py -numReduceTasks 0
